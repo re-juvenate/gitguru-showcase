3e661bf3258f501cd5836c079617ac25f9be60bd
Preetham Pemmasani <75422607+ppmpreetham@users.noreply.github.com>
2025-02-23 13:47:45 +0530

    ui fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,12 @@
 import { useState } from 'react';
-
+import Hero from './components/hero';
 function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <div className="w-screen h-screen flex justify-center items-center bg-black text-white">
-      <div className="flex flex-col items-center p-4">
-        <div className="text-8xl lg:text-[21rem] text-center text-yellow-300 font-departure tracking-tighter">
-          GITGURU
-        </div>
-        <div className="text-3xl text-center">
-          One stop solution to your github issues and PRs
-        </div>
-        <img 
-          src="https://github.com/re-juvenate/gitGuru-backend/raw/main/assets/logo.png" 
-          alt="GitGuru Logo"
-          className="lg:w-1/2"
-          style={{ imageRendering: 'pixelated' }}
-        />
-      </div>
-    </div>
+    <>
+      <Hero />
+    </>
   );
 }
 
