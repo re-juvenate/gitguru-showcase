de9fa143e393245e6ee3d32f269468f469f7e7ab
Preetham Pemmasani <75422607+ppmpreetham@users.noreply.github.com>
2025-02-23 12:03:53 +0530

    better hero

--- src/App.jsx
+++ src/App.jsx
@@ -4,18 +4,19 @@ function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <div className="w-screen h-screen text-center bg-black text-white">
-      <div className="flex flex-col p-4">
+    <div className="w-screen h-screen flex justify-center items-center bg-black text-white">
+      <div className="flex flex-col items-center p-4">
         <div className="text-8xl lg:text-[21rem] text-center text-yellow-300 font-departure tracking-tighter">
           GITGURU
         </div>
-        <div className="text-3xl">
+        <div className="text-3xl text-center">
           One stop solution to your github issues and PRs
         </div>
         <img 
           src="https://github.com/re-juvenate/gitGuru-backend/raw/main/assets/logo.png" 
           alt="GitGuru Logo"
-          className='lg:'
+          className="lg:w-1/2"
+          style={{ imageRendering: 'pixelated' }}
         />
       </div>
     </div>
